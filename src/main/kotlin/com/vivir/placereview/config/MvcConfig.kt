c60378d2a9fc4c2098dc015531b3628f395f76ca
Vivirinter <vivirequesty@gmail.com>
2021-12-17 19:32:37 +0300

    controller

---
+++ src/main/kotlin/com/vivir/placereview/config/MvcConfig.kt
@@ -0,0 +1,23 @@
+package com.vivir.placereview.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class MvcConfig : WebMvcConfigurerAdapter() {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry?.addViewController("/login")?.setViewName("login")
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry?.addResourceHandler(
+            "/webjars/**",
+            "/css/**")
+            ?.addResourceLocations(
+                "classpath:/META-INF/resources/webjars/",
+                "classpath:/static/css/")
+    }
+}
